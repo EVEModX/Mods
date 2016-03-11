c541e8279625b82021f2202c90965a7608342240
Yiming Wang <wangym5106@gmail.com>
2016-03-12 00:16:20 +0800

    Changed the method to reduce the impact on efficiency

--- mods/RemoveBannedWordsList/RemoveBannedWordsList.py
+++ mods/RemoveBannedWordsList/RemoveBannedWordsList.py
@@ -1,15 +1,4 @@
 import localization
-from localization.localizationBase import Localization as l10n
 
 def RunPatch():
-    if hasattr(l10n, "_EVEMODX_RemoveBannedWord_GetByLabel"):
-        return False
-    l10n._EVEMODX_RemoveBannedWord_GetByLabel = l10n.GetByLabel
-    def NewGetByLabel(self, labelNameAndPath, languageID = None, **kwargs):
-        if labelNameAndPath == "UI/Chat/ChannelWindow/ChinaServerBannedWords":
-            return ""
-        return l10n._EVEMODX_RemoveBannedWord_GetByLabel(self, labelNameAndPath, languageID, **kwargs)
-
-    l10n.GetByLabel = NewGetByLabel
-    localization.GetByLabel = localization.__GetLocalization().GetByLabel
-    return True
+    localization.LOCALIZATION_REF.languageLabels['UI/Chat/ChannelWindow/ChinaServerBannedWords'] = None
