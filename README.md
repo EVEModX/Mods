392371670e63170836c489dbc86d7139e5133bef
Yiming Wang <wangym5106@gmail.com>
2016-03-13 01:32:25 +0800

    Added mod: Auto target according to fleet broadcast

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Mods which can be injected to EVE exefile using EVEModX framework.
 - CustomThemeColor 自定义配色
 - RemoveBannedWordsList 聊天过滤反和谐
 - RemoveTiancityBanner 移除退出游戏显示的 banner
+- FleetAutoTarget 自动锁定广播目标
 
 ## Todo
-- FleetAutoTarget 自动锁定广播目标
 - RenameWormholes 替换虫洞名称 e.g. 虫洞Q003(通往零零)
 - RemoveTerransforce "未来人类"还原
 - FleetBroadcastRelay 转发当前舰队广播到指定聊天会话
---
+++ mods/FleetAutoTarget/AutoTarget.py
@@ -0,0 +1,18 @@
+import logmodule
+from eve.client.script.ui.shared.fleet.fleetbroadcast import FleetBroadcastView
+
+def PatchFn(fn):
+    def wrapper(self):
+        try:
+            br = sm.GetService('fleet').GetBroadcastHistory()[0]
+            logmodule.general.Log("GetBroadcastListEntry invoked: %s %d %d" % (br.name, br.charID, br.itemID), logmodule.LGNOTICE)
+            if br.name in ("Target", "HealArmor", "HealShield"):
+                sm.GetService('target').TryLockTarget(br.itemID)
+        except:
+            pass
+        return fn(self)
+    return wrapper
+
+def RunPatch():
+    FleetBroadcastView.LoadBroadcastHistory = PatchFn(FleetBroadcastView.LoadBroadcastHistory)
+    logmodule.general.Log("Code Injected", logmodule.LGNOTICE)
