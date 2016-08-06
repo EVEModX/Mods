2467c770cc281c0cf4f740eda894d433a863bf88
韩于惟 <hanyuwei70@qq.com>
2016-08-07 02:24:28 +0800

    AutoAlert 解决发送信息问题

--- mods/AutoAlert/AutoAlert.py
+++ mods/AutoAlert/AutoAlert.py
@@ -13,7 +13,7 @@ class AutoAlert(service.Service):
     __alertchannel = 0
 
     def OnLSC(self, channelID, estimatedMemberCount, method, identityInfo, args):
-        logmodule.general.Log("AutoAlert Processing....",logmodule.LGNOTICE)
+        logmodule.general.Log("AutoAlert Processing....method:%s chID:%s  id:%s"%(method,channelID,identityInfo),logmodule.LGNOTICE)
         AllianceID, CorpID, CfgLine, Role, CorpRole, WarFac = identityInfo
         if type(CfgLine) == types.IntType:
             CharID = CfgLine
@@ -22,18 +22,25 @@ class AutoAlert(service.Service):
         if method == "JoinChannel" and self.__alertchannel != 0:
             #判断是否为本地
             if type(channelID)==types.IntType:
+                logmodule.general.Log("Not Local CH",logmodule.LGNOTICE)
                 return
-            if channelID[0]!="solarsystemid2":
+            if channelID[0][0]!="solarsystemid2":
+                logmodule.general.Log("Not Local CH",logmodule.LGNOTICE)
                 return
             if self.Ishostile(CharID):  # 判断声望
-                sm.GetService('LSC').SendMessage(self.__alertchannel,"warning for"+CharID+"entering")
+                logmodule.general.Log("Sending Alert Message",logmodule.LGNOTICE)
+                msg="WARN: %s entering"%(str(CharID))
+                sm.GetService('LSC').SendMessage(self.__alertchannel,msg) #往服务器发送
+                sm.GetService('LSC').GetChannelWindow(self.__alertchannel).Speak(msg, eve.session.charid, localEcho=True) #本地聊天框刷新
+                logmodule.general.Log("Sent Alert Message",logmodule.LGNOTICE)
         elif method == "SendMessage":
             if CharID == session.charid:  # 发言的是自己才有效
                 if args[0] == ".startalert" and self.__alertchannel == 0:
-                    self.__alertchannel = channelID[1]
-                    logmodule.general.Log("Alert Channel Set",logmodule.LGNOTICE)
+                    self.__alertchannel = channelID
+                    logmodule.general.Log("Alert Channel Set. CH is %s"%str(self.__alertchannel),logmodule.LGNOTICE)
                 elif args[0] == ".stopalert":
                     self.__alertchannel = 0
+                    logmodule.general.Log("Alert Channel Cancelled",logmodule.LGNOTICE)
 
     def Ishostile(self, charid):  # 判断声望 True:报警 False:不报警
         # TODO:实现正确判断声望
